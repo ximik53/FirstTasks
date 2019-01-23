0b01c888ce985f543d259c9ea1d242fb2c8ee2b0
XiMiK <ximik53@yandex.ru>
2019-01-23 21:56:51 +0300

    Subsequence

---
+++ src/Subs/Subsequence.java
@@ -0,0 +1,23 @@
+package Subs;
+import java.util.*;
+
+public class Subsequence {
+    public <sub> boolean find(List <sub> first, List <sub> second) {
+        if (first.isEmpty() || second.isEmpty());
+        boolean verification = true;
+        int point = 0;
+        for (int firstIter = 0; firstIter < first.size(); firstIter++){
+            if (verification != true) break;
+            verification = false;
+            for (int secondIter = point; secondIter < second.size(); secondIter++){
+                if (first.get(firstIter).equals(second.get(secondIter))){
+                    verification = true;
+                    point = secondIter + 1;
+                    break;
+                }
+            }
+        }
+        return verification;
+    }
+}
+
---
+++ src/Subs/SubsequenceImpl.java
@@ -0,0 +1,11 @@
+package Subs;
+import java.util.*;
+
+public class SubsequenceImpl extends Subsequence {
+    public static void main(String[] args) {
+        Subsequence s = new SubsequenceImpl();
+        boolean b = s.find(Arrays.asList("A", "B", "C", "D"),
+                Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "D"));
+        System.out.println(b);
+    }
+}
